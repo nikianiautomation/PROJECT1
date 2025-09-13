c1095c9b90a72c72b37d918cfc19bf24975efe7c
nikiani <nikianiautomation@gmail.com>
2025-09-13 14:13:13 +0530

    updated name to ec3

--- ec2-terraform.tf
+++ ec2-terraform.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "ec2_example" {
     ami = "ami-0b09ffb6d8b58ca91"
     instance_type = "t3.micro"
     tags = {
-        Name = "terraform_ec2"
+        Name = "terraform_ec3"
     }
 }
