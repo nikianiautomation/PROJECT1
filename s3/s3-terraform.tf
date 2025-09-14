befcd53a1d270d49e74c22381906dc279ff2b3fd
nikiani <nikianiautomation@gmail.com>
2025-09-14 15:32:02 +0530

    s3 bucket creation and file upload

--- s3/s3-terraform.tf
+++ s3/s3-terraform.tf
@@ -3,7 +3,14 @@ provider "aws" {
     }   
 
 
-resource "aws_s3_bucket" "s3_object" {
-    bucket ="niki-ani1"
+resource "aws_s3_bucket" "s3_bucket3" {
+    bucket ="niki-ani3"
     acl = "private"
+}
+
+resource "aws_s3_bucket_object" "s3_object3" {
+    bucket = "niki-ani3"
+    key = "jenkins_installation.sh"
+    source = "../resources/jenkins_installation.sh"
+    etag = filemd5("../resources/jenkins_installation.sh")
 }
