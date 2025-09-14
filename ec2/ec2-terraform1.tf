febca5d3bc55760cf119fde3ca52e6565cf9c870
nikiani <nikianiautomation@gmail.com>
2025-09-14 12:21:52 +0530

    ec2 with jenkins

--- ec2/ec2-terraform1.tf
+++ ec2/ec2-terraform1.tf
@@ -2,11 +2,12 @@ provider "aws" {
     region = "us-east-1" 
     }
 resource "aws_instance" "ec2_example" {
-    ami = "ami-0360c520857e3138f"
+    ami = "ami-0b09ffb6d8b58ca91"
     instance_type = "t3.micro"
     key_name = "terraform_keypair"
     vpc_security_group_ids = ["sg-0f35ec365aafa7ff9"]
     tags = {
-       Name = "ec2_keypair"
+       Name = "ec2_jenkins"
     }
+    user_data = file("/resources/jenkins_installation.sh")
 }
